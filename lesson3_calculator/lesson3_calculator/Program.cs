6850d264099fd38a1229108e54f61f62f3a75cf3
bootnreboot <andrey.karamarinov@gmail.com>
2022-04-27 18:45:20 +0700

    rubius_lesson3_calculator

---
+++ lesson3_calculator/lesson3_calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите первое число:"); //ввод первого числа
+    var number1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите операцию (+ - / * %):"); //ввод операции
+    var operation = Console.ReadLine();
+
+    Console.WriteLine("Введите второе число:"); //ввод второго числа
+    var number2 = float.Parse(Console.ReadLine());
+
+
+    switch (operation)
+    {
+        case "+":
+            Console.WriteLine("Результат: " + (number1 + number2));
+            Console.WriteLine();
+            break;
+        case "-":
+            Console.WriteLine("Результат: " + (number1 - number2));
+            Console.WriteLine();
+            break;
+        case "/":
+            Console.WriteLine("Результат: " + (number1 / number2));
+            Console.WriteLine();
+            break;
+        case "*":
+            Console.WriteLine("Результат: " + (number1 * number2));
+            Console.WriteLine();
+            break;
+        case "%":
+            Console.WriteLine("Результат: "+ (number1 / number2) * 100);
+            Console.WriteLine();
+            break;
+        default:
+            Console.WriteLine("Ошибка! Попробуйте заново.");
+            Console.WriteLine();
+            break;
+    }
+}
